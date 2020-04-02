43fdce6ae71187614131d2a2c253daabe15c389f
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-02 15:40:48 -0400

    restructured data files and location

--- src/data/about-data.js
+++ src/data/about-data.js
@@ -1,10 +1,5 @@
 module.exports = {
-  email: "bjf216@gmail.com",
-  phone: "3038276224",
-  phoneDisplay: "303-827-6224",
   bio: "I am a full stack web developer with a passion for finding solutions to complex problems and the ability to see projects through from concept to completion.  My customer service skills honed in the financial services industry help me understand what clients need, and my experience at a fast-paced start-up in the games industry helps me get it done!  I am proficient in HTML, CSS, Bootstrap, JavaScript, jQuery, and Node.js, and am learning more every day.  Check out my work, and let me know if I can help you too!",
   photo: "/images/headshot.jpg",
   resume: "/files/BenFerriman_resume.pdf",
-  ghProfileURL: "https://github.com/bferriman",
-  linkedInURL: "https://www.linkedin.com/in/ben-ferriman/"
 };
---
+++ src/data/navbar-data.js
@@ -0,0 +1,7 @@
+module.exports = {
+  email: "bjf216@gmail.com",
+  phone: "3038276224",
+  phoneDisplay: "303-827-6224",
+  ghProfileURL: "https://github.com/bferriman",
+  linkedInURL: "https://www.linkedin.com/in/ben-ferriman/"
+};
