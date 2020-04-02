17fcee5da9ec5d4ddef936ac10f349547a7e2c2d
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-02 11:51:30 -0400

    adding data files

---
+++ data/projects.js
@@ -0,0 +1,52 @@
+module.exports = {
+  projects: [
+    {
+      name: "Friendango",
+      order: 1,
+      description: "No clout, just friends.  Read movie reviews from your friends without the distraction of chasing social media followers.",
+      screenshot: "/images/friendango.jpg",
+      deployedURL: "https://friendango.herokuapp.com",
+      githubURL: "https://github.com/grlucy/Friendango"
+    },  
+    {
+      name: "PetSpiration",
+      order: 2,
+      description: "Are you ready to give up on human companionship but not the fun of a dating app? Then PetSpiration is for you! We'll show you pictures of cats, you tell us whether you like them. Then we'll match you up with adoptable cats near you!",
+      screenshot: "/images/petspiration.jpg",
+      deployedURL: "https://bferriman.github.io/pet-spiration/",
+      githubURL: "https://github.com/bferriman/pet-spiration"
+    },
+    {
+      name: "Day Planner",
+      order: 4,
+      description: "Time to get organized! This app lets you schedule and save events. Uses Moment.js to responsively format time and date, and saves your events in Local Storage. Uses DOM manipulation to display state-appropriate button controls.",
+      screenshot: "/images/dayplanner.jpg",
+      deployedURL: "https://bferriman.github.io/day-planner/",
+      githubURL: "https://github.com/bferriman/day-planner"
+    },
+    {
+      name: "Weather Dashboard",
+      order: 3,
+      description: "Get the current weather as well as the five day forecast, for wherever you are, or wherever you may be going! Uses the OpenWeather API.",
+      screenshot: "/images/weather.jpg",
+      deployedURL: "https://bferriman.github.io/weather-dashboard/",
+      githubURL: "https://github.com/bferriman/weather-dashboard"
+    },
+    {
+      name: "Eat-The-Burger",
+      order: 6,
+      description: "A silly app, but a great way to practice MVC implementation. Create a burger, then eat it! Uses MySQL, Handlebars, Express, and a custom-built ORM.",
+      screenshot: "/images/eat-the-burger.jpg",
+      deployedURL: "https://limitless-ocean-97488.herokuapp.com/",
+      githubURL: "https://github.com/bferriman/eat-the-burger"
+    },
+    {
+      name: "Employee Template",
+      order: 5,
+      description: "A Node CLI for entering employee data for a software development team.  When done, it generates a styled HTML page to display your team.",
+      screenshot: "/images/emp-template.jpg",
+      deployedURL: "https://github.com/bferriman/employee-template",
+      githubURL: "https://github.com/bferriman/employee-template"
+    }
+  ]
+};
