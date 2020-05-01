da7888136899d79456f74b3a60e352c4648a655c
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-30 22:47:03 -0600

    updated skills listed in about section

--- src/data/about-data.js
+++ src/data/about-data.js
@@ -1,5 +1,5 @@
 module.exports = {
-  bio: "I am a full stack web developer with a passion for finding solutions to complex problems and the ability to see projects through from concept to completion.  My customer service skills honed in the financial services industry help me understand what clients need, and my experience at a fast-paced start-up in the games industry helps me get it done!  I am proficient in HTML, CSS, Bootstrap, JavaScript, jQuery, and Node.js, and am learning more every day.  Check out my work, and let me know if I can help you too!",
+  bio: "I am a full stack web developer with a passion for finding solutions to complex problems and the ability to see projects through from concept to completion. My customer service skills honed in the financial services industry help me understand what clients need, and my experience at a fast-paced start-up in the games industry helps me get it done! I am proficient in HTML, CSS, Bootstrap, Bulma, JavaScript, Node.js, Express, React, MongoDB, MySQL, and more, and I continue to learn every day. Check out my work, and let me know if I can help you too!",
   photo: "/images/headshot.jpg",
   resume: "/files/BenFerriman_resume.pdf",
 };
