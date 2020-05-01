20db3262fec68229a6f7df6d2a7f66e6f285e8b5
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-30 21:31:47 -0600

    cleaning up unused boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-// import "./App.css";
 import About from "./pages/About/About.js";
 import Portfolio from "./pages/Portfolio/Portfolio.js";
 
