cb9c775cd736d674fa747ccfb684d0cc6e622f58
Ben F <56423163+bferriman@users.noreply.github.com>
2020-04-30 22:48:03 -0600

    added latest project Dragon's Den to porfolio page

--- src/data/projects-data.js
+++ src/data/projects-data.js
@@ -1,16 +1,24 @@
 module.exports = {
   projects: [
     {
-      name: "Friendango",
+      name: "Dragon's Den",
       order: 1,
-      description: "No clout, just friends.  Read movie reviews from your friends without the distraction of chasing social media followers.",
+      description: "A local game store website for the age of COVID-19. Provides inventory management, online shopping, checkout, and curbside pickup, allowing a brick-and-mortar store to do business safely during the pandemic.",
+      screenshot: "/images/dragons-den.jpg",
+      deployedURL: "https://dragons-den-store.herokuapp.com",
+      githubURL: "https://github.com/grlucy/ecommerce-game-store"
+    },
+    {
+      name: "Friendango",
+      order: 2,
+      description: "No clout, just friends. Read movie reviews from your friends without the distraction of chasing social media followers.",
       screenshot: "/images/friendango.jpg",
       deployedURL: "https://friendango.herokuapp.com",
       githubURL: "https://github.com/grlucy/Friendango"
     },  
     {
       name: "PetSpiration",
-      order: 2,
+      order: 3,
       description: "Are you ready to give up on human companionship but not the fun of a dating app? Then PetSpiration is for you! We'll show you pictures of cats, you tell us whether you like them. Then we'll match you up with adoptable cats near you!",
       screenshot: "/images/petspiration.jpg",
       deployedURL: "https://bferriman.github.io/pet-spiration/",
@@ -18,7 +26,7 @@ module.exports = {
     },
     {
       name: "Day Planner",
-      order: 4,
+      order: 5,
       description: "Time to get organized! This app lets you schedule and save events. Uses Moment.js to responsively format time and date, and saves your events in Local Storage. Uses DOM manipulation to display state-appropriate button controls.",
       screenshot: "/images/dayplanner.jpg",
       deployedURL: "https://bferriman.github.io/day-planner/",
@@ -26,7 +34,7 @@ module.exports = {
     },
     {
       name: "Weather Dashboard",
-      order: 3,
+      order: 4,
       description: "Get the current weather as well as the five day forecast, for wherever you are, or wherever you may be going! Uses the OpenWeather API.",
       screenshot: "/images/weather.jpg",
       deployedURL: "https://bferriman.github.io/weather-dashboard/",
@@ -34,7 +42,7 @@ module.exports = {
     },
     {
       name: "Eat-The-Burger",
-      order: 6,
+      order: 7,
       description: "A silly app, but a great way to practice MVC implementation. Create a burger, then eat it! Uses MySQL, Handlebars, Express, and a custom-built ORM.",
       screenshot: "/images/eat-the-burger.jpg",
       deployedURL: "https://limitless-ocean-97488.herokuapp.com/",
@@ -42,7 +50,7 @@ module.exports = {
     },
     {
       name: "Employee Template",
-      order: 5,
+      order: 6,
       description: "A Node CLI for entering employee data for a software development team.  When done, it generates a styled HTML page to display your team.",
       screenshot: "/images/emp-template.jpg",
       deployedURL: "https://github.com/bferriman/employee-template",
