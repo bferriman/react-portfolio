ebb3ca6e0b69ea8eb898e49dcc70fe8d27273b6a
Ben F <56423163+bferriman@users.noreply.github.com>
2020-05-25 23:11:37 -0600

    updated email address

--- src/data/navbar-data.js
+++ src/data/navbar-data.js
@@ -1,7 +1,7 @@
 module.exports = {
-  email: "bjf216@gmail.com",
+  email: "benjamin.ferriman@gmail.com",
   phone: "3038276224",
   phoneDisplay: "303-827-6224",
   ghProfileURL: "https://github.com/bferriman",
-  linkedInURL: "https://www.linkedin.com/in/ben-ferriman/"
+  linkedInURL: "https://www.linkedin.com/in/ben-ferriman/",
 };
